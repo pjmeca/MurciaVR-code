e2885ca8cc2faae8b39cf6c0a4779bd287a04e92
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2023-07-01 11:55:46 +0200

    #45 Optimizar niebla

--- Assets/Scripts/Fog/Spawner.cs
+++ Assets/Scripts/Fog/Spawner.cs
@@ -1,20 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Inject]
+    private readonly GameObject Jugador;
+    private ParticleSystem _particleSystem;
+
+    /// <summary>
+    /// Distancia a partir de la cual deja de reproducirse el sistema de partículas.
+    /// </summary>
+    public static int DISTANCE_LIMIT = 700;
+
+    private void Start()
     {
-        
+        _particleSystem = gameObject.GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Obtener la distancia respecto al jugador       
+        float distancia = Vector3.Distance(Jugador.transform.position, gameObject.transform.position);
+
         // Si el jugador está a más de cierta distancia, parar el sistema de partículas
+        if (distancia > DISTANCE_LIMIT)
+        {
+            if(!_particleSystem.isStopped)
+            {
+                _particleSystem.Stop();
+            }                
+        }
         // Si está a menos, comprobar si el sistema de partículas se está reproduciendo y, si no, darle al play
-        // Tener en cuenta la vida de la niebla, si ya ha sido limpiada la zona, no hay que darle al play porque no hay nada que mostrar
+        else
+        {
+            if (!_particleSystem.isPlaying)
+            {
+                _particleSystem.Play();
+            }
+        }
     }
 }
